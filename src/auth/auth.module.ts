5cf5d444401ac0069c778e1fba18ea51d1cc9424
Harsh Harsola <harshharsola@gmail.com>
2024-06-18 18:58:46 +0530

    updated auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { User, UsersSchema } from 'src/schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: 'process.env.JWT_SECRET',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
