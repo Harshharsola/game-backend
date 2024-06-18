806d411dc47b5e482c5aba8669c5493811efc02b
Harsh Harsola <harshharsola@gmail.com>
2024-06-18 18:57:17 +0530

    added login endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { GameModule } from './game/game.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, GameModule],
+  imports: [
+    AuthModule,
+    GameModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/schemas/users.schema';
+import { UserSignInDto } from './dtos/userSignIn.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { getApiResponse } from 'src/utils';
 
-@Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+  async signIn(userSignInDto: UserSignInDto) {
+    try {
+      const user = await this.userModel.findOne({ email: userSignInDto.email });
+      if (!user) {
+        return getApiResponse({}, '400', 'wrong email');
+      }
+      const result = await bcrypt.compare(
+        userSignInDto.password,
+        user.password,
+      );
+
+      if (result) {
+        const payload = { sub: user._id, username: user.userName };
+        const accessToken = await this.jwtService.signAsync(payload);
+        return getApiResponse({ accessToken }, '200', 'login successfull');
+      } else {
+        return getApiResponse({}, '400', 'wrong password');
+      }
+    } catch (error) {
+      console.log(error);
+      return getApiResponse({}, '500', 'internal server error');
+    }
+  }
+}
